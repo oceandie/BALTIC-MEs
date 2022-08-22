214d3a8b2dd3eaba898f120e2faa27370f2eb17b
oceandie <diego.bruciaferri@metoffice.gov.uk>
2022-08-22 10:28:53 +0100

    removing unnecessary files

--- src/f90/NEMO_4.0-HEAD_test_MEs/tools/DOMAINcfg/BLD/fcm_env.ksh
+++
@@ -1,1 +0,0 @@
-/projects/jmmp/dbruciaf/NEMO/CHECKOUTS_RELEASE-4.0-HEAD/NEMO_4.0-HEAD_test_MEs/tools/DOMAINcfg/BLD/fcm_env.sh
--- src/f90/NEMO_4.0-HEAD_test_MEs/tools/DOMAINcfg/BLD/fcm_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-PATH=/projects/jmmp/dbruciaf/NEMO/CHECKOUTS_RELEASE-4.0-HEAD/NEMO_4.0-HEAD_test_MEs/tools/DOMAINcfg/BLD/bin:$PATH
-export PATH
