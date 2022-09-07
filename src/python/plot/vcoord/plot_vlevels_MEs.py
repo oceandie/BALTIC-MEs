7d7b53c43d85ec303c71412bbce7e018621634fc
oceandie <diego.bruciaferri@metoffice.gov.uk>
2022-09-07 09:26:06 +0100

    adding code to plot vertical coord cross-sections

---
+++ src/python/plot/vcoord/plot_vlevels_MEs.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import numpy as np
+import xarray as xr
+from xnemogcm import open_domain_cfg
+from plot_section import mpl_sec_loop
+from utils import compute_masks
+
+# ========================================================================
+# INPUT PARAMETERS
+
+DOMCFG_MEs = '/data/users/dbruciaf/CO9_AMM15/dom_cfg/domain_cfg_MEs_01-003_opt_v2.nc'
+BATHY_MEs = '/home/h01/dbruciaf/mod_dev/CO_AMM15/BUILD_CFG/MEs_envs/G-E-G-NICO_BLOCK_River_10mMIN.amm15_MEs_2env_r01-003_opt_v2.nc'
+
+# 2. ANALYSIS
+
+sec_lon1  = [-17.617,-3.975,9.747]    # AMM15 south boundary
+sec_lat1  = [44.065,45.195,44.461]    # AMM15 south boundary
+
+sec_I_indx_1b_L  = [sec_lon1]
+sec_J_indx_1b_L  = [sec_lat1]
+
+coord_type_1b_L  = "dist"
+rbat2_fill_1b_L  = "false"
+xlim_1b_L        = "maxmin"
+ylim_1b_L        = [0., 1000.] #1000.] #3500.] #5900.]
+vlevel_1b_L      = 'MES'
+xgrid_1b_L       = "false"
+
+# ========================================================================
+# Reading local-MEs mask
+msk_mes = None
+ds_msk = xr.open_dataset(BATHY_MEs)
+hbatt = []
+nenv = 1
+while nenv > 0:
+  name_env = "hbatt_"+str(nenv)
+  if name_env in ds_msk.variables:
+      hbatt.append(ds_msk[name_env].values)
+      nenv+=1
+  else:
+      nenv=0
+del ds_msk
+
+# Loading domain geometry
+ds_dom  = open_domain_cfg(files=[DOMCFG_MEs])
+for i in ['bathymetry','bathy_meter']:
+    for dim in ['x','y']:
+        ds_dom[i] = ds_dom[i].rename({dim: dim+"_c"})
+
+# Computing masks
+ds_dom = compute_masks(ds_dom, merge=True)
+
+tlon2 = ds_dom["glamt"].values
+tlat2 = ds_dom["gphit"].values
+e3t_3 = ds_dom["e3t_0"].values
+e3w_3 = ds_dom["e3w_0"].values
+tmsk3 = ds_dom["tmask"].values
+bathy = ds_dom["bathymetry"].values
+
+nk = e3t_3.shape[0]
+nj = e3t_3.shape[1]
+ni = e3t_3.shape[2]
+
+tlon3 = np.repeat(tlon2[np.newaxis, :, :], nk, axis=0)
+tlat3 = np.repeat(tlat2[np.newaxis, :, :], nk, axis=0)
+
+# Computing model levels' depth
+tdep3 = np.zeros(shape=(nk,nj,ni))
+wdep3 = np.zeros(shape=(nk,nj,ni))
+wdep3[0,:,:] = 0.
+tdep3[0,:,:] = 0.5 * e3w_3[0,:,:]
+for k in range(1, nk):
+    wdep3[k,:,:] = wdep3[k-1,:,:] + e3t_3[k-1,:,:]
+    tdep3[k,:,:] = tdep3[k-1,:,:] + e3w_3[k,:,:]
+
+proj = []
+
+# PLOTTING VERTICAL DOMAIN
+
+var_strng  = ""
+unit_strng = ""
+date       = ""
+timeres_dm = ""
+timestep   = []
+PlotType   = ""
+var4       = []
+#hbatt      = []
+mbat_ln    = "false"
+mbat_fill  = "true"
+varlim     = "no"
+check      = 'true'
+check_val  = 'false'
+
+
+mpl_sec_loop('AMM15 mesh', '.png', var_strng, unit_strng, date, timeres_dm, timestep, PlotType,
+              sec_I_indx_1b_L, sec_J_indx_1b_L, tlon3, tlat3, tdep3, wdep3, tmsk3, var4, proj,
+              coord_type_1b_L, vlevel_1b_L, bathy, hbatt, rbat2_fill_1b_L, mbat_ln, mbat_fill,
+              xlim_1b_L, ylim_1b_L, varlim, check, check_val, xgrid_1b_L, msk_mes=msk_mes)
+
+
